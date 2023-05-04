94290f1ddf6c2cd1062dea9d82b1e18c03cfbc86
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-05-04 20:40:05 +0900

    enginesを追加

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/techpit-vue-tetris/'
-    : '/'
+  outputDir: 'docs',
+  assetsDir: './',
+  publicPath: './'
 })
