56b60c5bfa0dde81a871d28803f15682e35d1b81
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-05-04 19:32:21 +0900

    初期デプロイ

---
+++ src/pages/PlayPage.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <h1>プレイ画面</h1>
+  <h2>ユーザ名: {{ $route.query.name }}</h2>
+  <h2>ユーザ名: {{ $route.query.money }}</h2>
+</template>
+
+<style>
+</style>
