caf78df352e76550c7514e1292cfd642994a83bf
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-05-04 20:11:16 +0900

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  outputDir: 'docs',
-  assetsDir: './',
-  publicPath: './'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/techpit-vue-tetris/'
+    : '/'
 })
